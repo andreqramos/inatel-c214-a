5dd75e4a3ae4c397a3fbe437dd644451cdcd5a4f
André Ramos <andreqramos@hotmail.com>
2022-10-18 18:57:24 -0300

    Refactor code to optimize number 3 logic

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,8 +1,4 @@
 def fizzbuzz(number):
-    if number == 9:
-        return 'fizz'
-    if number == 6:
-        return 'fizz'
-    if number == 3:
+    if number in [9, 6, 3]:
         return 'fizz'
     return str(number)
